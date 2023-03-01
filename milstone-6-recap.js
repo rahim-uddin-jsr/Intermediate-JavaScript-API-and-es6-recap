41f4720888d71aac30f2a4ab7fcb68f8a78d68fb
dev-rahim <rahimuddin5915@gmail.com>
2023-03-01 07:55:25 +0600

    problem 2 solved

--- milstone-6-recap.js
+++ milstone-6-recap.js
@@ -21,3 +21,11 @@ const diploma = {
 
 const result = diploma.returnSomething();
 console.log(result);
+
+/* ২. একটা টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার করো। সেটার মধ্যে উপরের অবজেক্ট এর একটা তিনটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। এর মধ্যে একটা প্রপার্টি সেট করবে--যেটা নেস্টেড অবজেক্ট আছে সেটার প্রপার্টি থেকে। আরেকটা প্রপার্টি হবে যে প্রপার্টি এর মান একটা array সেটার সেকেন্ড পজিশন এর উপাদান দিয়ে। আর বাকি একটা তোমার ইচ্ছা মতো বসাতে পারো।
+ */
+
+const showDiplomaDetails = `
+My institute Name is ${diploma.instituteName}. I read in ${diploma.technicalLabs.softwareLab}. In our Institute The department of ${diploma.departmentName[1]} students are very Active in curriculum Activities.
+`;
+console.log(showDiplomaDetails);
